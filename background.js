db7cc898b32135b50eee86fa342549680177d99d
Alexandre Alencar <alencardev@gmail.com>
2021-12-25 14:49:15 +0100

    close tab if it contains one of the blocked keywords in its url

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
 init()
 
 async function init() {
-  chrome.tabs.onUpdated.addListener(async () => {
-    const { blocked } = await chrome.storage.sync.get({ blocked: [] })
-    const currentTab = await chrome.tabs.query({ active: true })
-    if (blocked.find(b => currentTab.url && currentTab.url.includes(b))) {
-      chrome.tabs.remove(currentTab.id)
+  chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    const { blockList } = await chrome.storage.sync.get({ blockList: [] })
+    if (blockList.find(b => tab.url.includes(b.website))) {
+      chrome.tabs.remove(tab.id)
     }
   })
 }
